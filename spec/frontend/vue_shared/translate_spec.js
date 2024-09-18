2aaa3629c3e4fa2cbb94a7bfa08bbffe3ff2e036
root <root@localhost>
2024-09-18 16:05:49 +0000

    Update from merge request

--- spec/frontend/vue_shared/translate_spec.js
+++ spec/frontend/vue_shared/translate_spec.js
@@ -1,14 +1,42 @@
-import { mount } from '@vue/test-utils';
+import { mount, WrapperArray } from '@vue/test-utils';
 import Vue from 'vue';
 import locale from '~/locale';
 import Translate from '~/vue_shared/translate';
+import Component from './translate_spec.vue';
+
+/**
+ * Huh. It seems like that findAll behave differently in the Vue Utils for Vue2 and Vue3.
+ *
+ * The Vue2 Version is not iterable and exposes the actual elements under .wrappers.
+ *
+ * Wonder if we should do it like this and where to put it and whether there are more instances of
+ *
+ * findAll
+ */
+if (!WrapperArray.prototype[Symbol.iterator]) {
+  WrapperArray.prototype[Symbol.iterator] = function iterator() {
+    let index = -1;
+    const elements = this.wrappers;
+    return {
+      next() {
+        index += 1;
+        return { done: index >= elements.length, value: elements[index] };
+      },
+    };
+  };
+}
 
 Vue.use(Translate);
 
 describe('Vue translate filter', () => {
-  const createTranslationMock = (key, ...translations) => {
-    locale.textdomain('app');
+  let oldDomain;
+  let oldData;
 
+  beforeAll(() => {
+    oldDomain = locale.textdomain();
+    oldData = locale.options.locale_data;
+
+    locale.textdomain('app');
     locale.options.locale_data = {
       app: {
         '': {
@@ -16,172 +44,33 @@ describe('Vue translate filter', () => {
           lang: 'vo',
           plural_forms: 'nplurals=2; plural=(n != 1);',
         },
-        [key]: translations,
+        singular: ['singular_translated'],
+        plural: ['plural_singular translation', 'plural_multiple translation'],
+        '%d day': ['%d singular translated', '%d plural translated'],
+        'Context|Foobar': ['Context|Foobar translated'],
+        'multiline string': ['multiline string translated'],
+        'multiline plural': ['multiline string singular', 'multiline string plural'],
+        'Context| multiline string': ['multiline string with context'],
       },
     };
-  };
-
-  it('translate singular text (`__`)', () => {
-    const key = 'singular';
-    const translation = 'singular_translated';
-    createTranslationMock(key, translation);
-
-    const wrapper = mount({
-      template: `
-          <span>
-            {{ __('${key}') }}
-          </span>
-        `,
-    });
-
-    expect(wrapper.text()).toBe(translation);
   });
 
-  it('translate plural text (`n__`) without any substituting text', () => {
-    const key = 'plural';
-    const translationPlural = 'plural_multiple translation';
-    createTranslationMock(key, 'plural_singular translation', translationPlural);
-
-    const wrapper = mount({
-      template: `
-          <span>
-            {{ n__('${key}', 'plurals', 2) }}
-          </span>
-        `,
-    });
-
-    expect(wrapper.text()).toBe(translationPlural);
+  afterAll(() => {
+    locale.textdomain(oldDomain);
+    locale.options.locale_data = oldData;
   });
 
-  describe('translate plural text (`n__`) with substituting %d', () => {
-    const key = '%d day';
+  it('works properly', async () => {
+    const wrapper = await mount(Component);
 
-    beforeEach(() => {
-      createTranslationMock(key, '%d singular translated', '%d plural translated');
-    });
+    // vue3 vs vue2
+    const spans = wrapper.findAll('span');
 
-    it('and n === 1', () => {
-      const wrapper = mount({
-        template: `
-            <span>
-              {{ n__('${key}', '%d days', 1) }}
-            </span>
-          `,
-      });
+    // Just to ensure that the rendering actually worked;
+    expect(spans.length).toBe(10);
 
-      expect(wrapper.text()).toBe('1 singular translated');
-    });
-
-    it('and n > 1', () => {
-      const wrapper = mount({
-        template: `
-            <span>
-              {{ n__('${key}', '%d days', 2) }}
-            </span>
-          `,
-      });
-
-      expect(wrapper.text()).toBe('2 plural translated');
-    });
-  });
-
-  describe('translates text with context `s__`', () => {
-    const key = 'Context|Foobar';
-    const translation = 'Context|Foobar translated';
-    const expectation = 'Foobar translated';
-
-    beforeEach(() => {
-      createTranslationMock(key, translation);
-    });
-
-    it('and using two parameters', () => {
-      const wrapper = mount({
-        template: `
-            <span>
-              {{ s__('Context', 'Foobar') }}
-            </span>
-          `,
-      });
-
-      expect(wrapper.text()).toBe(expectation);
-    });
-
-    it('and using the pipe syntax', () => {
-      const wrapper = mount({
-        template: `
-            <span>
-              {{ s__('${key}') }}
-            </span>
-          `,
-      });
-
-      expect(wrapper.text()).toBe(expectation);
-    });
-  });
-
-  it('translate multi line text', () => {
-    const translation = 'multiline string translated';
-    createTranslationMock('multiline string', translation);
-
-    const wrapper = mount({
-      template: `
-          <span>
-            {{ __(\`
-            multiline
-            string
-            \`) }}
-          </span>
-        `,
-    });
-
-    expect(wrapper.text()).toBe(translation);
-  });
-
-  it('translate pluralized multi line text', () => {
-    const translation = 'multiline string plural';
-
-    createTranslationMock('multiline string', 'multiline string singular', translation);
-
-    const wrapper = mount({
-      template: `
-          <span>
-            {{ n__(
-            \`
-            multiline
-            string
-            \`,
-            \`
-            multiline
-            strings
-            \`,
-            2
-            ) }}
-          </span>
-        `,
-    });
-
-    expect(wrapper.text()).toBe(translation);
-  });
-
-  it('translate pluralized multi line text with context', () => {
-    const translation = 'multiline string with context';
-
-    createTranslationMock('Context| multiline string', translation);
-
-    const wrapper = mount({
-      template: `
-          <span>
-            {{ s__(
-            \`
-            Context|
-            multiline
-            string
-            \`
-            ) }}
-          </span>
-        `,
-    });
-
-    expect(wrapper.text()).toBe(translation);
+    for (const span of spans) {
+      expect(span.text().trim()).toBe(span.attributes()['data-expected']);
+    }
   });
 });
