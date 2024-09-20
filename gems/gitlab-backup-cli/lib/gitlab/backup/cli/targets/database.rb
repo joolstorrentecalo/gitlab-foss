07cbdab0365cd1006547e1df2be76d392f8cc4dc
root <root@localhost>
2024-09-20 07:16:04 +0000

    Update from merge request

--- gems/gitlab-backup-cli/lib/gitlab/backup/cli/targets/database.rb
+++ gems/gitlab-backup-cli/lib/gitlab/backup/cli/targets/database.rb
@@ -5,10 +5,6 @@ module Gitlab
     module Cli
       module Targets
         class Database < Target
-          # TODO: Refactor to remove coupling with compress and decompress commands
-          # https://gitlab.com/gitlab-org/gitlab/-/issues/454830
-          include ::Backup::Helper
-
           attr_reader :force, :errors
 
           IGNORED_ERRORS = [
@@ -117,7 +113,7 @@ module Gitlab
               pg_env = backup_connection.database_configuration.pg_env_variables
               success = with_transient_pg_env(pg_env) do
                 decompress_rd, decompress_wr = IO.pipe
-                decompress_pid = spawn(decompress_cmd, out: decompress_wr, in: db_file_name)
+                decompress_pid = spawn(Gitlab::Bacup::Cli.decompression_command, out: decompress_wr, in: db_file_name)
                 decompress_wr.close
 
                 status, tracked_errors =
--- gems/gitlab-backup-cli/spec/gitlab/backup/cli_spec.rb
+++ gems/gitlab-backup-cli/spec/gitlab/backup/cli_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe Gitlab::Backup::Cli do
   it "has a version number" do
     expect(Gitlab::Backup::Cli::VERSION).not_to be nil
   end
+
+  describe '.compression_command' do
+    it 'returns a Shell::Command instance for gzip compression' do
+      command = described_class.compression_command
+
+      expect(command).to be_a(Gitlab::Backup::Cli::Shell::Command)
+      expect(command.cmd_args).to eq(['gzip', '-c', '-1'])
+    end
+  end
+
+  describe '.decompression_command' do
+    it 'returns a Shell::Command instance for gzip decompression' do
+      command = described_class.decompression_command
+
+      expect(command).to be_a(Gitlab::Backup::Cli::Shell::Command)
+      expect(command.cmd_args).to eq(['gzip', '-cd'])
+    end
+  end
 end
